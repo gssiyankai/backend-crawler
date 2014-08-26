218f8f9963ea1d8c621826db843f72273d1d82fc
gssiyankai <syk.greg@gmail.com>
2014-08-26 23:20:26 +0200

    wip

--- maven.groovy
+++ maven.groovy
@@ -19,11 +19,11 @@ def listFromURL(url) {
     def pattern = Pattern.compile("maven-([0-9\\.]+)(-bin)?.zip\$")
 
     return p.getAnchors().collect { HtmlAnchor a ->
-        m = pattern.matcher(a.hrefAttribute)
-        if(m.find()) {
-            ver=m.group(1)
-            url = p.getFullyQualifiedUrl(a.hrefAttribute)
-            return ["id":ver, "name":ver, "url":url.toExternalForm()]
+        def m = pattern.matcher(a.hrefAttribute)
+        if (m.find()) {
+            def ver = m.group(1)
+            def fqUrl = p.getFullyQualifiedUrl(a.hrefAttribute)
+            return ["id": ver, "name": ver, "url": fqUrl.toExternalForm()]
         }
         return null;
     }
@@ -33,28 +33,35 @@ def listFromOldURL() {
     return listFromURL("http://archive.apache.org/dist/maven/binaries/")
 }
 
-def listFromNewUrl() {
+def listMavenFoldersFromNewUrl() {
     def HtmlPage p = getHtmlPage("http://archive.apache.org/dist/maven/")
     def pattern = Pattern.compile("maven-([0-9])/\$")
-    def pattern1 = Pattern.compile("([0-9\\.])+/\$")
-    def pattern2 = Pattern.compile("^binaries/\$")
 
     return p.getAnchors().collect { HtmlAnchor a ->
         def m = pattern.matcher(a.hrefAttribute)
-        if(m.matches()) {
+        if (m.matches()) {
             def url = p.getFullyQualifiedUrl(a.hrefAttribute)
-            def HtmlPage p1 = getHtmlPage(url)
-            p1.getAnchors().collect { HtmlAnchor a1 ->
-                def m1 = pattern1.matcher(a1.hrefAttribute)
-                if (m1.matches()) {
-                    def url1 = p1.getFullyQualifiedUrl(a1.hrefAttribute)
-                    def HtmlPage p2 = getHtmlPage(url1)
-                    p2.getAnchors().collect { HtmlAnchor a2 ->
-                        def url2 = p2.getFullyQualifiedUrl(a2.hrefAttribute)
-                        def m2 = pattern2.matcher(a2.hrefAttribute)
-                        if (m2.matches()) {
-                            return listFromURL(url2)
-                        }
+            return getHtmlPage(url)
+        }
+        return null
+    }.findAll { it != null }
+}
+
+def listFromNewUrl() {
+    def pattern1 = Pattern.compile("([0-9\\.])+/\$")
+    def pattern2 = Pattern.compile("^binaries/\$")
+
+    return listMavenFoldersFromNewUrl().collect { HtmlPage p1 ->
+        p1.getAnchors().collect { HtmlAnchor a1 ->
+            def m1 = pattern1.matcher(a1.hrefAttribute)
+            if (m1.matches()) {
+                def url1 = p1.getFullyQualifiedUrl(a1.hrefAttribute)
+                def HtmlPage p2 = getHtmlPage(url1)
+                p2.getAnchors().collect { HtmlAnchor a2 ->
+                    def url2 = p2.getFullyQualifiedUrl(a2.hrefAttribute)
+                    def m2 = pattern2.matcher(a2.hrefAttribute)
+                    if (m2.matches()) {
+                        return listFromURL(url2)
                     }
                 }
             }
@@ -64,7 +71,7 @@ def listFromNewUrl() {
 
 def listAll() {
     return (listFromOldURL() + listFromNewUrl())
-            .findAll { it!=null }.unique().sort { o1,o2 ->
+            .findAll { it != null }.unique().sort { o1, o2 ->
         try {
             def v1 = new VersionNumber(o1.id)
             try {
@@ -83,9 +90,9 @@ def listAll() {
     }
 }
 
-def store(key,o) {
+def store(key, o) {
     JSONObject envelope = JSONObject.fromObject(["list": o])
-    lib.DataWriter.write(key,envelope)
+    lib.DataWriter.write(key, envelope)
 }
 
 store("hudson.tasks.Maven.MavenInstaller", listAll())
